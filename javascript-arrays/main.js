7bf898391bfcff4ecd5bb075cd60fbfea0e55e93
Youssef Najjarine <youssefnajjarine@hotmail.com>
2020-12-16 10:43:41 -0800

    I made the changes you asked of me.

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -9,7 +9,12 @@ console.log('Mexico is ' + colors[0] + ',' + colors[1] + ',' + 'and ' + colors[2
 console.log('The value of colors: ' + colors);
 
 var students = ['John', 'Mike', 'Dave', 'Steve'];
-var numberOfStudents = [students.length];
+var numberOfStudents = students.length;
 console.log('There are ' + numberOfStudents + ' students in the class.');
-console.log('The last student in the array is ' + students[3] + '.');
-console.log('value of students: ' + students);
+
+var lastIndex = numberOfStudents - 1;
+console.log(lastIndex);
+
+var lastStudent = students[3];
+console.log('The last student in the array is ' + lastStudent);
+console.log('The value of students: ' + students);
