9dc9c284325cd51f73afbeef3ad20ef9f0a77162
Youssef Najjarine <youssefnajjarine@hotmail.com>
2020-12-15 19:48:22 -0800

    Made changes to typeof and line 10.

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -2,14 +2,14 @@ var fullName = 'Youssef Najjarine';
 var isCool = false;
 var totalPets = 1;
 var unknown = null;
-var never = ' ';
+var never =
 console.log(fullName);
 console.log(isCool);
 console.log(totalPets);
 console.log(unknown);
 console.log(never);
-console.log(typeof 'Youssef Najjarine');
-console.log(typeof false);
-console.log(typeof 1);
+console.log(typeof fullName);
+console.log(typeof isCool);
+console.log(typeof totalPets);
 console.log(typeof unknown);
 console.log(typeof never);
