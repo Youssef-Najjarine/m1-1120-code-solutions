ed0a4fdcb8e1490e1ab3eeee69b4bc9c7607a9a9
Youssef Najjarine <youssefnajjarine@hotmail.com>
2020-12-15 21:07:35 -0800

    Completed javascript-arrays.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,15 @@
+var colors = ['red', 'white', 'blue'];
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log('America is ' + colors[0] + ',' + colors[1] + ',' + 'and ' + colors[2] + '.');
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ',' + colors[1] + ',' + 'and ' + colors[2] + '.');
+console.log('The value of colors: ' + colors);
+
+var students = ['John', 'Mike', 'Dave', 'Steve'];
+var numberOfStudents = [students.length];
+console.log('There are ' + numberOfStudents + ' students in the class.');
+console.log('The last student in the array is ' + students[3] + '.');
+console.log('value of students: ' + students);
